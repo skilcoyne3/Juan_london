dbe0733b531522f2b062ffed2239d87e0976ab4e
Shawn Kilcoyne <sbsk94@gmail.com>
2017-02-27 16:43:04 -0500

    merge-stream and browserify

--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,1 +1,14 @@
 //JS FROM scripts.js
+
+///*****   Libraries added   *****///
+global.jQuery = require('jquery');
+bootstrap = require('bootstrap');
+mustache = require('mustache');
+
+///*****  end Libraries *****///
+
+
+///***** My Code *****///
+jQuery(document).ready(function(){
+ alert('hello');
+});
